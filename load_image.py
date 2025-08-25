b6465f52695adb88e468ca0b8068b3f308c95d97
jirayu_wannagul@hotmail.com <jirayu_wannagulhotmail.com@MacBook-Air-khxng-jirayu-wannagulhotmailcom.local>
2025-08-25 11:04:49 +0700

    fix: แก้ไขไฟล์ที่ถูก modified ทั้งหมด

--- load_image.py
+++ load_image.py
@@ -1,9 +1,9 @@
 import cv2
 
-img = cv2.imread("wave_chart.png")
+# เปลี่ยนชื่อผู้ใช้เป็นของคุณจริง ๆ (เช่น jirayu_wannagulhotmail.com)
+img = cv2.imread("/Users/jirayu_wannagulhotmail.com/Desktop/wave_chart.png")
 
 if img is None:
-    print("❌ ไม่เจอไฟล์รูป")
+    print("❌ ไม่เจอไฟล์รูป ลองเช็ค path อีกครั้ง")
 else:
     print("✅ โหลดรูปสำเร็จ ขนาด:", img.shape)
-
